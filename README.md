10bf893cca9eb0717fa1f5520942afc25d0e4a64
Firas Hasan <x.firashasan@gmail.com>
2021-03-30 20:14:23 +0300

    because i added more content and made the files more organized

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
 # new-course
+
+## lab:02 About Me, with a guessing game
+
+the repo URL
+https://github.com/FirasHasan/new-course
+live URL:
+https://firashasan.github.io/new-course/
+
+1. it went great, i enjoyed this lab.
+
+2. it's that all my time estimations on doing assignments is always wrong xD. and no i have no qestions everything's great.
+
+3. it took me about 2 and a half hours, i thought i can make it in 30 mins
+
+## Lab: 03 - Extend your 'About Me' and guessing game
+i added a lot of things:
+- added the numbers game and the user has 4 attempts.
+- added a list of colors in an array and the user has to guess my favorite color with only has 6 attempts.
+- added 7 questions to the user.
+- made a counter that counts the score of the user depending on how many answers he got right.
+- in html i added an ordered list for the about me part.
+- then i added an unordered list that talks about 10 of my top 10 songs.
+- i also added the reset.css file.
+- and added the external style.css file instead of keeping it internel, and linked it with my html file.
+- added  a new folder that has the external javascript file instead of keeping in it, internel and linked it with my html file.
+- added the eslinct file.
+- made a new folder to put both reset.css file and style.css inside it.
+
+## learned about 
+- we revised the switch statement again
+- talked about arrays
+- used if else statements
+- logical operators
+    - Logical and
+    - Logical or
+    - Logical not
+- comprarison operators such as:
+    - strict equal
+    - not strict equal
+    - bigger than
+    - smaller than
+    - bigger than equals
+    - smaller than equals    
+
+- truthy and falsy values: are to check if a variable has a value or not
+    - can be NaN, undefined, empty string, null.
+- loops :
+    - for loops if we know when the loop is going to stop.
+    - while loops if we dont know the stopping condition of the loop.
+    - do while loops : is used mostly to test hardware. the code will at least run once.       
+- continue and break
+    - continue skips the the rest of the code for that iteration then continue back to the rest.
+    - break to break out of all loop.
+- reset.css does which is it refreshes everything before the normal style.css applies to html    
--- js/app.js
+++ js/app.js
@@ -7,18 +7,23 @@ let fifthQuestion = prompt("do i like psychology? yes/no y/n");
 let sixthQuestion = prompt("do you think i'm a cat person and not a dog person? yes/no y/n");
 let seventhQuestion = prompt("do you think i like music? yes/no y/n");
 
+
+
 let ageAnswer = "";
 let thirdAnswer = "";
 let fourthAnswer = "";
 let fifthAnswer = "";
 let sixthAnswer = "";
 let seventhAnswer = "";
+let eighthAnswer = "a";
+let score = 0;
 
 
 console.log("nice meeting you " + userName);
 if (ageQuestion == 22) {
     ageAnswer = "you were right i am 22";
     console.log(ageAnswer);
+    score++;
 }
 else {
     ageAnswer = " but you were wrong i am 22 years old";
@@ -32,12 +37,14 @@ if (thirdQuestion.toLowerCase() == "yes" || thirdQuestion.toLowerCase() == "y") 
 else {
     thirdAnswer = "you were right i used to be a barista before getting into web-development";
     console.log(thirdAnswer);
+    score++;
 
 }
 
 if (fourthQuestion.toLowerCase() == "yes" || fourthQuestion.toLowerCase() == "y") {
     fourthAnswer = "you're right i do wear glasses";
     console.log(fourthAnswer);
+    score++;
 }
 else {
     fourthAnswer = "you are wrong i do wear glasses";
@@ -47,6 +54,7 @@ else {
 if (fifthQuestion.toLowerCase() == "yes" || fifthQuestion.toLowerCase() == "y") {
     fifthAnswer = "you're right i love psychology";
     console.log(fifthAnswer);
+    score++;
 }
 else {
     fifthAnswer = "you are wrong i love psychology";
@@ -57,6 +65,7 @@ if (sixthQuestion.toLowerCase() == "yes" || sixthQuestion.toLowerCase() == "y") 
 
     sixthAnswer = "you're right i am a cat person";
     console.log(sixthAnswer);
+    score++;
 }
 else {
     sixthAnswer = "you're wrong i'm a cat person";
@@ -66,16 +75,54 @@ else {
 if (seventhQuestion.toLowerCase() == "yes" || seventhQuestion.toLowerCase() == "y") {
     seventhAnswer = "true, i listen to music 24/7";
     console.log(seventhAnswer);
+    score++;
 }
 else {
     seventhAnswer = "you are wrong, i do love music!";
     console.log(seventhAnswer);
 }
 
+
+eighthQuestion = ["white", "yellow", "green", "blue", "black", "red", "orange", "brown"];
+let j = 1;
+while (eighthAnswer !== "green" && j !== 6) {
+    if (eighthAnswer == "green") { score++; }
+    eighthAnswer = prompt("of these what do you think my favorite color is? \n" + eighthQuestion);
+
+    console.log("your answer is " + eighthAnswer);
+    j++;
+}
+alert("the right answer is green");
+
+console.log("the right answer is green!");
+
 alert("nice meeting you " + userName + "\n"
-    + "your think i'm " + ageQuestion + " years old , " + ageAnswer + "\n"
+    + "you think i'm " + ageQuestion + " years old , " + ageAnswer + "\n"
     + "your answer is " + thirdQuestion + " , " + thirdAnswer + "\n"
     + "your answer is " + fourthQuestion + " , " + fourthAnswer + "\n"
     + "your answer is " + fifthQuestion + " , " + fifthAnswer + "\n"
     + "you answered " + sixthQuestion + " , " + sixthAnswer + "\n"
-    + "you answered " + seventhQuestion + " , " + seventhAnswer);
+    + "you answered " + seventhQuestion + " , " + seventhAnswer + "\n"
+    + "my favorite color is green " + " your last answer was: " + eighthAnswer + "\n"
+    + "your score is: " + score);
+
+
+alert("let's play a game i call the lucky number \n you have 4 chances to guess the number ");
+let luckyNumber = 0;
+let counter = 0;
+for (let i = 1; i <= 4; i++) {
+    luckyNumber = prompt("pick a number 1-10");
+    if (luckyNumber < 9)
+        alert("your guess is too low try something higher");
+    else if (luckyNumber > 9)
+        alert("your guess is too high, try something lower");
+    else if (luckyNumber = 9) {
+        alert("you're right! the lucky number is 9");
+        break;
+    }
+    counter++;
+    alert("guess number: " + counter);
+
+}
+
+
